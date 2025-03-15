d98bfc4dda7147e6414b2ec3c20f27e4d523912b
roger18gm <roger18gm@users.noreply.github.com>
2025-03-15 17:34:02 +0000

    deploy: 188b5c6bd4da1f4cbc4e59aa973cc887e234feb4

---
+++ assets/main-CRuc9tHl.js
@@ -0,0 +1,1 @@
+import"./style-B7iV-Qrq.js";const t=()=>{const e=document.querySelector("#toggle-nav-btn"),o=document.querySelector(".menu-svg"),n=document.querySelector(".close-svg");e.addEventListener("click",()=>{document.querySelector(".menu-dropdown").classList.toggle("show"),o.classList.toggle("hide"),n.classList.toggle("hide")})};t();
