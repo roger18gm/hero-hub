8b433afbc13387a9f2669c15a0c4dcdefec2db81
caleb-wo <caleb-wo@users.noreply.github.com>
2025-04-07 06:06:58 +0000

    deploy: 71cbd5470188fc90ec3c481a307f0f9173fb4808

--- assets/main-DPk-Iy-Y.js
+++
@@ -1,6 +0,0 @@
-import{o as r,h as t,g as s}from"./api-Cdm6-UTf.js";import{g as n}from"./favoriteHero-Cj1f5rFi.js";r();t();const c=async e=>(console.log("In hero favorites to objects."),Promise.all(e.map(o=>s(o)))),i=async()=>{console.log("In hero card maker.");const e=await c(n());if(e.length){const o=document.querySelector("#favoritedList");o.insertAdjacentHTML("beforebegin",'<h2 id="favoriteListTitle">Favorite Heroes</h2>'),e.forEach(a=>{o.insertAdjacentHTML("beforeend",`
-                <div class="heroCard">
-                    <a href="focus.html?heroId=${a.id}"><img class="cardImage" alt="Image of hero" src="${a.image.original_url}"></a>
-                    <h2 class="cardHeroName">${a.name}</h2>
-                </div> 
-            `)})}};document.addEventListener("DOMContentLoaded",async()=>{await i()});
