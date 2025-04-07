d2ca677407e33480aaa294ae3991956d98069aa0
caleb-wo <camawolfe52@icloud.com>
2025-04-06 20:38:46 -0600

    Finished favorite hero list.

--- src/scripts/heroBookmarkBar.mjs
+++ src/scripts/heroBookmarkBar.mjs
@@ -9,15 +9,19 @@ const convertHeroIdsToObjects = async( favoriteHeroIds ) =>{
 const generateFavoriteHeroCards = async() =>{
     console.log("In hero card maker.");
     const favoriteHeros = await convertHeroIdsToObjects( getFavoriteHeroIds() );
-    const cardBox = document.querySelector("#favoritedList");
-    favoriteHeros.forEach(hero => {
-        cardBox.insertAdjacentHTML("beforeend", `
-            <div class="heroCard">
-                <img class="cardImage" alt="Image of hero" src="${hero.image.original_url}">
-                <h2 class="cardHeroName">${hero.name}</h2>
-            </div> 
-        `)
-    });
+    if (favoriteHeros.length){
+        const cardBox = document.querySelector("#favoritedList");
+        cardBox.insertAdjacentHTML("beforebegin", "<h2 id=\"favoriteListTitle\">Favorite Heroes</h2>")
+
+        favoriteHeros.forEach(hero => {
+            cardBox.insertAdjacentHTML("beforeend", `
+                <div class="heroCard">
+                    <a href="focus.html?heroId=${hero.id}"><img class="cardImage" alt="Image of hero" src="${hero.image.original_url}"></a>
+                    <h2 class="cardHeroName">${hero.name}</h2>
+                </div> 
+            `)
+        });
+    }
 }
 
 document.addEventListener("DOMContentLoaded", async() =>{ await generateFavoriteHeroCards() });
