344aeb79645d611f1b66ba8cd02bad589a890c5e
caleb-wo <caleb-wo@users.noreply.github.com>
2025-04-07 02:38:58 +0000

    deploy: d2ca677407e33480aaa294ae3991956d98069aa0

--- assets/main-Wh4W-gLm.js
+++
@@ -1,6 +0,0 @@
-import{o as r,h as s,g as t}from"./api-TbsWuuHg.js";import{g as n}from"./favoriteHero-Cj1f5rFi.js";r();s();const c=async e=>(console.log("In hero favorites to objects."),Promise.all(e.map(o=>t(o)))),d=async()=>{console.log("In hero card maker.");const e=await c(n()),o=document.querySelector("#favoritedList");e.forEach(a=>{o.insertAdjacentHTML("beforeend",`
-            <div class="heroCard">
-                <img class="cardImage" alt="Image of hero" src="${a.image.original_url}">
-                <h2 class="cardHeroName">${a.name}</h2>
-            </div> 
-        `)})};document.addEventListener("DOMContentLoaded",async()=>{await d()});
