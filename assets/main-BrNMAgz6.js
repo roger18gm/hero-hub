02f94550da594094faa5127ea84536ff8b3268a9
caleb-wo <caleb-wo@users.noreply.github.com>
2025-04-08 17:27:56 +0000

    deploy: 0cdc815c10542de0e5391c09002ed12b9c28cba2

---
+++ assets/main-BrNMAgz6.js
@@ -0,0 +1,6 @@
+import{o as r,h as t,g as s}from"./api-DrmbX5Sf.js";import{g as n}from"./favoriteHero-Cj1f5rFi.js";r();t();const c=async e=>{if(console.log("In hero favorites to objects."),e!==null)return Promise.all(e.map(o=>s(o)))},i=async()=>{console.log("In hero card maker.");const e=await c(n());if(e){const o=document.querySelector("#favoritedList");o.insertAdjacentHTML("beforebegin",'<h2 id="favoriteListTitle">Favorite Heroes</h2>'),e.forEach(a=>{o.insertAdjacentHTML("beforeend",`
+                <div class="heroCard">
+                    <a href="focus.html?heroId=${a.id}"><img class="cardImage" alt="Image of hero" src="${a.image.original_url}"></a>
+                    <h2 class="cardHeroName">${a.name}</h2>
+                </div> 
+            `)})}};document.addEventListener("DOMContentLoaded",async()=>{await i()});
