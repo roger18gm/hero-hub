f9b0809b3d7828ae332ae7060da833e0b3bb9a68
caleb-wo <caleb-wo@users.noreply.github.com>
2025-04-08 19:16:01 +0000

    deploy: f7c7dc4152e1749543d675672a005240f3e8b62d

--- assets/search-pNqLbHu2.js
+++
@@ -1,7 +0,0 @@
-import{b as a,o as t,h as c}from"./api-DrmbX5Sf.js";const o=document.getElementById("search-results");document.addEventListener("DOMContentLoaded",()=>{const e=new URLSearchParams(window.location.search).get("query");if(e)try{n(e),console.log("success?")}catch(s){console.error(s)}else o.textContent="No query provided."});const n=async r=>{try{const e=await a(r);e?(console.log("hero data: ",e),console.log(e.results[0]),d(e.results)):console.error("Error: Hero data not found or error occurred.")}catch(e){console.error("request failed: ",e)}};t();c();const l=r=>{const e=`focus.html?heroId=${r.id}`;return`<section class="hero-card">
-                <h2 class="hero-name">${r.name}</h2>
-                <img class="hero-card-img" src="${r.image.small_url}" alt="hero image"/>
-                <div class="link-div">
-                <a class="card-link" href="${e}">Visit hero profile!</a>
-                </div>
-            </section> `},d=r=>{const e=r.map(l);o.insertAdjacentHTML("beforeend",e.join(""))};
